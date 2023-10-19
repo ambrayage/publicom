392b6e552db43e5c6b61a489cbe633e6f0b05a96
LenyBl <blee.leny@gmail.com>
2023-10-19 11:31:51 +0200

    ajouter un message en base de données

--- app/Views/pages/create_messages.php
+++ app/Views/pages/create_messages.php
@@ -12,27 +12,28 @@ $this->section('content');
 
 
 <div class="container border rounded">
-    <form action="">
+    <form method="post" action="<?= url_to('creation.message'); ?>">
         <div class="text-center">
             <div class="input-group ">
                 <span class="mt-5 input-group-text" id="basic-addon1">Titre</span>
                 <input type="text" class="mt-5 form-control" placeholder="Titre du message" aria-label="Username"
-                    aria-describedby="basic-addon1">
+                    aria-describedby="basic-addon1" name="titre">
             </div>
         </div>
         <div class="text-center">
             <div class="input-group">
                 <span class="mt-4 input-group-text">Contenu ( 160 caractères max )</span>
-                <textarea class="mt-4 form-control" aria-label="With textarea"></textarea>
+                <textarea class="mt-4 form-control" aria-label="With textarea" name="contenu"></textarea>
             </div>
         </div>
         <div class="form-check  form-switch">
-            <input class="mt-5 form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+            <input class="mt-5 form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" name="statut" checked>
             <label class="mt-5 form-check-label " for="flexSwitchCheckChecked">Afficher ?</label>
         </div>
         <input class="mt-3 mb-4 w-100 btn btn-primary " type="submit" value="Ajouter">
 
     </form>
+   
 </div>
 
 <?= $this->endSection(); ?>
