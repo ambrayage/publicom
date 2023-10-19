d187fc1916cd0d5b2db02c94513bd13d8c418245
LenyBl <blee.leny@gmail.com>
2023-10-19 17:23:36 +0200

    modification en cours

--- app/Controllers/MessageController.php
+++ app/Controllers/MessageController.php
@@ -93,7 +93,6 @@ class MessageController extends BaseController
         }
 
         $data = [
-            'IDMESSAGE' => $idMessage,
             'TITREMESSAGE' => $titreModifier,
             'TEXTEMESSAGE' => $contenuModifier,
             'DATEHEUREMESSAGE' => $dateHeure,
