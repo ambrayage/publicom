1e38f7c5ae66f2da314013539c9a30a81c5acb65
DocteurMoriarty <128460992+DocteurMoriarty@users.noreply.github.com>
2023-10-19 09:56:01 +0200

    f

--- app/Views/pages/visualisation_messages.php
+++ app/Views/pages/visualisation_messages.php
@@ -1,10 +1,10 @@
 <?= $this->extend('layout_hf') ?>
 <?= $this->section('content') ?>
+<main>
+  <div class="card">
+    P
+  </div>
+</main>
 
-<div class="card">
-  Magic Card
-</div>
 
 <?= $this->endSection() ?>
-
-
